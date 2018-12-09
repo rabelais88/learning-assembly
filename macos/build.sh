a44f8d285d7382b391cc8d0d169c9dfd432fbf81
sungryeol <rabelais@bigpi.co>
2018-12-10 05:02:13 +0900

    first commit

---
+++ macos/build.sh
@@ -0,0 +1,10 @@
+echo "building NASM source from MacOS 2018"
+if [ $# -eq 0 ] || [ -z "$1" ]; then
+    echo "need file name as argument"
+fi
+# build object file from source
+nasm -f macho64 $1.s
+# link object file to executable file
+# ld -o <output>
+ld -macosx_version_min 10.7.0 -lSystem -o $1 $1.o
+echo "build finished"
